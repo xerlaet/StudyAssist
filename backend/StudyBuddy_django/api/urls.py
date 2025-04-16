72b68a15036d76adca53107cb9e52a38a29e5fb1
SaiAPydimarrry <151088249+SaiAPydimarrry@users.noreply.github.com>
2025-04-15 20:48:13 -0500

    CRUD for accounts done

--- backend/StudyBuddy_django/api/urls.py
+++ backend/StudyBuddy_django/api/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
-from .views_account import getAccount
+from .views_account import getAccounts, getAccount, createAccount, updateAccount, deleteAccount
 
 urlpatterns = [
-    path('accounts/', getAccount, name='getAccount')
+    path('accounts/', getAccounts, name='getAccounts'),  # Get all accounts
+    path('accounts/create/', createAccount, name='createAccount'),  # Create a new account
+    path('accounts/<str:email>/', getAccount, name='getAccount'),  # Get a specific account by email
+    path('accounts/update/<str:email>/', updateAccount, name='updateAccount'),  # Update an account by email
+    path('accounts/delete/<str:email>/', deleteAccount, name='deleteAccount'),  # Delete an account by email
 ]
