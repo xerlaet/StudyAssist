3d24300fd95ac7c9c567833b445bdc9c3df33a0c
Nicholas Gover <narwhalusa@gmail.com>
2025-04-20 18:34:00 -0500

    Fixed and added test cases for account and userData

--- backend/StudyBuddy_django/api/tests.py
+++ backend/StudyBuddy_django/api/tests.py
@@ -1,3 +1,117 @@
 from django.test import TestCase
+from django.urls import reverse
 
 # Create your tests here.
+class APITestCase(TestCase):
+    def setUp(self):
+        # Set up the test client
+        self.client = self.client_class()
+        self.client.post(path='/api/account_manage/create/', data={'username': 'jack', 'password': 'black', 'email': 'jack@black.com'})
+        self.client.post(path='/api/account_manage/create/', data={'username': 'sean', 'password': 'combs', 'email': 'diddy@babyoil.com'})
+        self.client.post(path='/api/account_manage/create/', data={'username': 'jordan', 'password': 'cartier', 'email': 'playboi@carti.com'})
+        self.client.post(path='/api/user_data_manage/create/', data={'email': 'diddy@babyoil.com', 'days': 0, 'streak': 0, 'calendar': {}})
+        self.client.post(path='/api/user_data_manage/create/', data={'email': 'jack@black.com', 'days': 13, 'streak': 7, 'calendar': {}})
+
+    """ def test_get_accounts(self):
+        # Simulate getting all accounts
+        response = self.client.get(path='/api/accounts/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_get_account_by_email_valid(self):
+        # Simulate getting an account by email
+        response = self.client.get(path='/api/accounts/jack@black.com/')
+        self.assertEqual(response.status_code, 200)
+    
+    def test_get_account_by_email_invalid(self):
+        # Simulate getting an account by invalid email
+        response = self.client.get(path='/api/accounts/chicken@jockey.com/')
+        self.assertEqual(response.status_code, 404)
+
+    def test_create_account(self):
+        # Simulate creating an account
+        response = self.client.post(path='/api/account_manage/create/', data={'username': 'chicken', 'password': 'jockey', 'email': 'chicken@jockey.com'})
+        self.assertEqual(response.status_code, 201)
+    
+    def test_create_account_invalid(self):
+        # Simulate creating an account with invalid data
+        response = self.client.post(path='/api/account_manage/create/', data={'chicken jockey': 'throw popcorn'})
+        self.assertEqual(response.status_code, 400)
+    
+    def test_update_account_valid(self):
+        # Simulate updating an account
+        response = self.client.put(path='/api/account_manage/update/playboi@carti.com/', data={'username': 'liar', 'password': 'liar', 'email': 'playboi@carti.com'}, content_type='application/json')
+        self.assertEqual(response.status_code, 200)
+    
+    def test_update_account_invalid_404(self):
+        # Simulate updating an account with invalid email
+        response = self.client.put(path='/api/account_manage/update/chicken@jockey.com/', data={'username': 'Harold', 'password': 'Bryne', 'email': 'chicken@jockey.com'}, content_type='application/json')
+        self.assertEqual(response.status_code, 404)
+    
+    def test_update_account_invalid_400(self):
+        # Simulate updating an account with invalid data
+        response = self.client.put(path='/api/account_manage/update/diddy@babyoil.com/', data={'felonies': 'three'}, content_type='application/json')
+        self.assertEqual(response.status_code, 400)
+    
+    def test_delete_account_valid(self):
+        # Simulate deleting an account
+        response = self.client.delete(path='/api/account_manage/delete/jack@black.com/')
+        self.assertEqual(response.status_code, 204)
+    
+    def test_delete_account_invalid(self):
+        # Simulate deleting an account with invalid email
+        response = self.client.delete(path='/api/account_manage/delete/chicken@jockey.com/')
+        self.assertEqual(response.status_code, 404) """
+    
+    def test_create_user_data_valid(self):
+        # Simulate creating user data
+        response = self.client.post(path='/api/user_data_manage/create/', data={'email': 'playboi@carti.com', 'days': 1000, 'streak': 50, 'calendar': {}})
+        self.assertEqual(response.status_code, 201)
+    
+    def test_create_user_data_invalid(self):
+        # Simulate creating user data with invalid data
+        response = self.client.post(path='/api/user_data_manage/create/', data={'chicken jockey': 'throw popcorn'})
+        self.assertEqual(response.status_code, 400)
+    
+    def test_create_user_data_exists(self):
+        # Simulate creating user data that already exists
+        response = self.client.post(path='/api/user_data_manage/create/', data={'email': 'diddy@babyoil.com', 'days': 0, 'streak': 0, 'calendar': {}})
+    
+    def test_get_user_data(self):
+        # Simulate getting all user data
+        response = self.client.get(path='/api/user_data/')
+        self.assertEqual(response.status_code, 200)
+    
+    def test_get_user_data_by_email_valid(self):
+        # Simulate getting user data by email
+        response = self.client.get(path='/api/user_data/diddy@babyoil.com/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_get_user_data_by_email_invalid(self):
+        # Simulate getting user data by invalid email
+        response = self.client.get(path='/api/user_data/chicken@jockey.com/')
+        self.assertEqual(response.status_code, 404)
+    
+    def test_update_user_data_valid(self):
+        # Simulate updating user data
+        response = self.client.put(path='/api/user_data_manage/update/jack@black.com/', data={'email': 'jack@black.com', 'days': 1000, 'streak': 50, 'calendar': {}}, content_type='application/json')
+        self.assertEqual(response.status_code, 200)
+    
+    def test_update_user_data_invalid_404(self):
+        # Simulate updating user data with invalid email
+        response = self.client.put(path='/api/user_data_manage/update/chicken@jockey.com/', data={'email': 'chicken@jockey.com', 'days': 1000, 'streak': 50, 'calendar': {}}, content_type='application/json')
+        self.assertEqual(response.status_code, 404)
+    
+    def test_update_user_data_invalid_400(self):
+        # Simulate updating user data with invalid data
+        response = self.client.put(path='/api/user_data_manage/update/jack@black.com/', data={'chicken jockey': 'throw popcorn'}, content_type='application/json')
+        self.assertEqual(response.status_code, 400)
+    
+    def test_delete_user_data_valid(self):
+        # Simulate deleting user data
+        response = self.client.delete(path='/api/user_data_manage/delete/jack@black.com/')
+        self.assertEqual(response.status_code, 204)
+    
+    def test_delete_user_data_invalid(self):
+        # Simulate deleting user data with invalid email
+        response = self.client.delete(path='/api/user_data_manage/delete/chicken@jockey.com/')
+        self.assertEqual(response.status_code, 404)
--- backend/StudyBuddy_django/api/views_userData.py
+++ backend/StudyBuddy_django/api/views_userData.py
@@ -2,8 +2,9 @@ from django.shortcuts import render
 from rest_framework.decorators import  api_view
 from rest_framework.response import  Response
 from rest_framework import  status
-from .models import UserData
+from .models import UserData, Account
 from .serializer import UserDataSerializer
+from django.utils.datastructures import MultiValueDictKeyError
 
 @api_view(['GET'])
 def get_user_data(request):
@@ -12,14 +13,21 @@ def get_user_data(request):
 @api_view(['GET'])
 def get_user_data_by_email(request, email):
     try:
-        obj = UserData.objects.get(email=email)
+        obj = UserData.objects.get(email=Account.objects.get(email=email).pk)
         return Response(UserDataSerializer(obj).data)
     except UserData.DoesNotExist:
+        return Response({'detail': 'Account not found'}, status=status.HTTP_404_NOT_FOUND)
+    except Account.DoesNotExist:
         return Response({'detail': 'Account not found'}, status=status.HTTP_404_NOT_FOUND)
 
 @api_view(['POST'])
 def create_user_data(request):
-    serializer = UserDataSerializer(data=request.data)
+    data_copy = request.data.copy() # Convert Django QueryDict to a regular dict
+    try:
+        data_copy['email'] = Account.objects.get(email=request.data['email']).pk
+    except MultiValueDictKeyError:
+        return Response({'detail': 'Invalid data in request'}, status=status.HTTP_400_BAD_REQUEST)
+    serializer = UserDataSerializer(data=data_copy)
     if serializer.is_valid():
         serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
@@ -28,10 +36,19 @@ def create_user_data(request):
 @api_view(['PUT'])
 def update_user_data(request, email):
     try:
-        obj = UserData.objects.get(email=email)
+        obj = UserData.objects.get(email=Account.objects.get(email=email).pk) # Get UserData by email
     except UserData.DoesNotExist:
         return Response({'detail': 'Account not found'}, status=status.HTTP_404_NOT_FOUND)
-    serializer = UserDataSerializer(obj, data=request.data)
+    except Account.DoesNotExist:
+        return Response({'detail': 'Account not found'}, status=status.HTTP_404_NOT_FOUND)
+    data_copy = request.data.copy() # Convert Django QueryDict to a regular dict
+    try:
+        data_copy['email'] = Account.objects.get(email=request.data['email']).pk # Get the email from the request data
+    except MultiValueDictKeyError:
+        return Response({'detail': 'Invalid data in request'}, status=status.HTTP_400_BAD_REQUEST)
+    except KeyError:
+        return Response({'detail': 'Invalid data in request'}, status=status.HTTP_400_BAD_REQUEST)
+    serializer = UserDataSerializer(obj, data=data_copy)
     if serializer.is_valid():
         serializer.save()
         return Response(serializer.data)
@@ -40,8 +57,10 @@ def update_user_data(request, email):
 @api_view(['DELETE'])
 def delete_user_data(request, email):
     try:
-        obj = UserData.objects.get(email=email)
+        obj = UserData.objects.get(email=Account.objects.get(email=email).pk)
         obj.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
     except UserData.DoesNotExist:
+        return Response({'detail': 'Account not found'}, status=status.HTTP_404_NOT_FOUND)
+    except Account.DoesNotExist:
         return Response({'detail': 'Account not found'}, status=status.HTTP_404_NOT_FOUND)
