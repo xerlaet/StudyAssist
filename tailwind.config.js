1635b9d9e32e083ec4869513210993415a79ffd3
Dshah1003 <sdrashti.10jan@gmail.com>
2025-04-29 23:00:09 -0500

    Detail Stats updated

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,8 +13,20 @@ module.exports = {
             800: '#3a3a3a',  // card background
             700: '#4a4a4a',  // alternate gray for accents
           },
+          primary: '#2dd4bf' // match Mathematics bar color
         },
+        height: {
+          'bar-1': '16%',
+          'bar-1_5': '24%',
+          'bar-1_8': '28%',
+          'bar-2': '32%',
+          'bar-2_2': '36%',
+          'bar-2_5': '40%',
+          'bar-3_2': '64%',
+          'bar-4': '100%'
+        }
       },
     },
     plugins: [],
   };
+  
